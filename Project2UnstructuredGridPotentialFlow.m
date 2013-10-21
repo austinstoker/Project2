3804d936cbc09df0b2960bfcf3608bab5d12fd61
austinstoker <puyuch@Austins-MacBook-Pro.local>
2013-10-21 17:15:42 -0600

    Working for velocities

--- Project2UnstructuredGridPotentialFlow.m
+++ Project2UnstructuredGridPotentialFlow.m
@@ -5,8 +5,13 @@ close all
 clearvars
 clc
 
-mycase=3;
+mycase=4;
 DoPlots=false;
+
+a=1/2;
+U=1;
+Gam = 4*pi*a*U;
+Gam_hat=Gam/(2*pi);
 
 if mycase==1 
     load circle16Edges.mat
@@ -29,60 +34,22 @@ numPoints=size(pointCoords,1);
 numCells=size(edges,1);
 
 
-nodeVolumes = zeros(numCells,1);
-if DoPlots
-    figure(1)
-    hold on;
-end
+xfun = @(x,y) x;
+phiHalfBodyfun = @(x,y) U*y-U*(a^2)*y/(x^2+y^2);
 
 
-for i=1:numCells
-    use4=false;
-    n1 = edges(i,1);
-    n2 = edges(i,2);
-    n3 = edges(i,3);
-    n4 = edges(i,4);
-    %isInterior = edges(i,5);
-    
-    x1 = pointCoords(n1,1);
-    x2 = pointCoords(n2,1);
-    x3 = pointCoords(n3,1);
-    
-    y1 = pointCoords(n1,2);
-    y2 = pointCoords(n2,2);
-    y3 = pointCoords(n3,2);
-    
-    p1 = pointCoords(n1,:);
-    p2 = pointCoords(n2,:);
-    p3 = pointCoords(n3,:);
-    
-    nodeVolumes(n3) = nodeVolumes(n3)+1/2*(x1+x2)*(y2-y1);
-    
-    
-    if n4~=0
-        x4 = pointCoords(n4,1);
-        y4 = pointCoords(n4,2);
-        p4 = pointCoords(n4,:);
-        use4=true;
-        
-        nodeVolumes(n4) = nodeVolumes(n4)-1/2*(x1+x2)*(y2-y1);
-        if DoPlots
-            plot([p1(1) p4(1)],[p1(2) p4(2)])
-            plot([p4(1) p2(1)],[p4(2) p2(2)])
-            plot([p2(1) p3(1)],[p2(2) p3(2)])
-            plot([p3(1) p1(1)],[p3(2) p1(2)])
-        end
-    else
-        use4=false;
-        
-        nodeVolumes(n1) = nodeVolumes(n1)+1/2*(x1+x2)*(y2-y1);
-        nodeVolumes(n2) = nodeVolumes(n2)+1/2*(x1+x2)*(y2-y1);
-        if DoPlots
-            plot([p1(1) p2(1)],[p1(2) p2(2)])
-            plot([p2(1) p3(1)],[p2(2) p3(2)])
-            plot([p3(1) p1(1)],[p3(2) p1(2)])
-        end
-    end
-end
-sum(nodeVolumes)/3
+phifun = @(x,y) U*(sqrt(x^2+y^2)-((a)^2)/sqrt(x^2+y^2))*y+Gam_hat*log(sqrt(x^2+y^2)/a);
+
+V0 = get_dfdx_hat(pointCoords,edges,xfun);
+
+dfdx = get_dfdx_hat(pointCoords,edges,phifun)./V0;
+dfdy = -1*get_dfdy_hat(pointCoords,edges,phifun)./V0;
+
+u=dfdy;
+v=-dfdx;
+
+figure(1)
+%surf(pointCoords(:,1),pointCoords(:,2),u);
+figure(2);
+quiver(pointCoords(:,1),pointCoords(:,2),u/10,v/10,'autoscale','off');
     
---
+++ get_dfdx_hat.m
@@ -0,0 +1,61 @@
+function [ dfdx ] = get_dfdx_hat( pointCoords,edges,fun )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%DoPlots=false;
+numPoints=size(pointCoords,1);
+numCells=size(edges,1);
+dfdx = zeros(numPoints,1);
+
+for i=1:numCells
+    %use4=false;
+    n1 = edges(i,1);
+    n2 = edges(i,2);
+    n3 = edges(i,3);
+    n4 = edges(i,4);
+    %isInterior = edges(i,5);
+    
+    x1 = pointCoords(n1,1);
+    x2 = pointCoords(n2,1);
+    %x3 = pointCoords(n3,1);
+    
+    y1 = pointCoords(n1,2);
+    y2 = pointCoords(n2,2);
+    %y3 = pointCoords(n3,2);
+    
+    %p1 = pointCoords(n1,:);
+    %p2 = pointCoords(n2,:);
+    %p3 = pointCoords(n3,:);
+    
+    dfdx(n3) = dfdx(n3)+1/2*(fun(x1,y1)+fun(x2,y2))*(y2-y1);
+    
+    
+    if n4~=0
+        %x4 = pointCoords(n4,1);
+        %y4 = pointCoords(n4,2);
+        %p4 = pointCoords(n4,:);
+        %use4=true;
+        
+        dfdx(n4) = dfdx(n4)-1/2*(fun(x1,y1)+fun(x2,y2))*(y2-y1);
+%         if DoPlots
+%             plot([p1(1) p4(1)],[p1(2) p4(2)])
+%             plot([p4(1) p2(1)],[p4(2) p2(2)])
+%             plot([p2(1) p3(1)],[p2(2) p3(2)])
+%             plot([p3(1) p1(1)],[p3(2) p1(2)])
+%         end
+    else
+        %use4=false;
+        
+        dfdx(n1) = dfdx(n1)+1/2*(fun(x1,y1)+fun(x2,y2))*(y2-y1);
+        dfdx(n2) = dfdx(n2)+1/2*(fun(x1,y1)+fun(x2,y2))*(y2-y1);
+%         if DoPlots
+%             plot([p1(1) p2(1)],[p1(2) p2(2)])
+%             plot([p2(1) p3(1)],[p2(2) p3(2)])
+%             plot([p3(1) p1(1)],[p3(2) p1(2)])
+%         end
+    end
+end
+
+
+end
+
